11e6bccc9f4ad30eddbe67e6bae5289eeaf0b22a
David Sichau <mail@sichau.eu>
2012-05-29 15:47:46 +0200

    Extracted common types in own header file

---
+++ libs/MPILib/BasicTypes.hpp
@@ -0,0 +1,20 @@
+/*
+ * BasicTypes.hpp
+ *
+ *  Created on: 29.05.2012
+ *      Author: david
+ */
+
+#ifndef BASICTYPES_HPP_
+#define BASICTYPES_HPP_
+
+
+typedef int Algorithm;
+typedef int NodeType;
+typedef int NodeId;
+typedef double WeightType;
+typedef int SimulationRunParameter;
+typedef int Node;
+typedef int Time;
+
+#endif /* BASICTYPES_HPP_ */
