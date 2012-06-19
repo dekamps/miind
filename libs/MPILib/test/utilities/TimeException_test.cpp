7a463c7f80119cdadd80084ffbe05745fb324e88
David Sichau <mail@sichau.eu>
2012-06-19 12:53:08 +0200

    Added test for time exception

---
+++ libs/MPILib/test/utilities/TimeException_test.cpp
@@ -0,0 +1,51 @@
+/*
+ * TimeException_test.cpp
+ *
+ *  Created on: 01.06.2012
+ *      Author: david
+ */
+
+#include <MPILib/include/utilities/TimeException.hpp>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+
+#include <boost/test/minimal.hpp>
+using namespace boost::unit_test;
+using namespace MPILib::utilities;
+
+namespace mpi = boost::mpi;
+
+mpi::communicator world;
+
+void test_Constructor() {
+	TimeException e("message");
+	std::stringstream sstream;
+	sstream << "message";
+
+	BOOST_REQUIRE(
+			strncmp(sstream.str().c_str(), e.what(), sstream.str().size())== 0);
+	TimeException e2(std::string("message"));
+	BOOST_REQUIRE(
+			strncmp(sstream.str().c_str(), e2.what(), sstream.str().size())== 0);
+}
+
+int test_main(int argc, char* argv[]) // note the name!
+		{
+
+	mpi::environment env(argc, argv);
+
+	if (world.size() != 2) {
+		BOOST_FAIL( "Run the test with two processes!");
+	}
+
+	// we use only two processors for this testing
+	test_Constructor();
+
+	return 0;
+
+}
+
