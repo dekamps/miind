324984c84c6402d45e68483254fd2f787b5e5993
David Sichau <mail@sichau.eu>
2012-06-28 11:40:45 +0200

    Added first Algorithm needed for two population test

---
+++ apps/Miind-mpi/two-population.cpp
@@ -0,0 +1,174 @@
+/*
+ * two-population.cpp
+ *
+ *  Created on: 28.06.2012
+ *      Author: david
+ */
+
+#include <MPILib/include/MPINodeCode.hpp>
+#include <MPILib/include/MPINetworkCode.hpp>
+#include <MPILib/include/utilities/CircularDistribution.hpp>
+#include <MPILib/include/algorithm/RateFunctor.hpp>
+
+namespace mpi = boost::mpi;
+using namespace MPILib;
+
+const Rate RATE_TWOPOPULATION_EXCITATORY_BACKGROUND = 2.0; // Hz
+const double TWOPOPULATION_FRACTION = 0.5;
+
+const double TWOPOPULATION_C_E = 20000;
+const double TWOPOPULATION_C_I = 2000;
+
+const Efficacy TWOPOPULATION_J_EE = 20e-3 / 170.0;
+const Efficacy TWOPOPULATION_J_IE = 20e-3 / 70.15;
+
+const double g = 3.0;
+
+const Efficacy TWOPOPULATION_J_EI = g * TWOPOPULATION_J_EE;
+const Efficacy TWOPOPULATION_J_II = g * TWOPOPULATION_J_IE;
+
+const Time TWOPOPULATION_TIME_BEGIN = 0; // 0 sec
+const Time TWOPOPULATION_TIME_END = 0.05; // 1 sec
+const Time TWOPOPULATION_TIME_REPORT = 1e-3; // 10 ms
+const Time TWOPOPULATION_TIME_UPDATE = 1e-2; // 100 ms
+const Time TWOPOPULATION_TIME_NETWORK = 1e-6; // 0.1 ms
+
+const PopulistParameter
+TWOPOPULATION_NETWORK_EXCITATORY_PARAMETER_POP(
+		TWOPOPULATION_NETWORK_EXCITATORY_PARAMETER, TWOPOP_SPECIFIC);
+
+const PopulistParameter
+TWOPOPULATION_NETWORK_INHIBITORY_PARAMETER_POP(
+		TWOPOPULATION_NETWORK_INHIBITORY_PARAMETER, TWOPOP_SPECIFIC);
+
+inline Rate CorticalBackground(Time t) {
+	return RATE_TWOPOPULATION_EXCITATORY_BACKGROUND;
+}
+
+template<class Algorithm, class WeightValue, class NodeDistribution>
+MPINetwork<WeightValue, NodeDistribution> CreateTwoPopulationNetwork //Edited by Johannes: namespacequalifier removed
+(
+		NodeId* p_id_cortical_background, //
+		NodeId* p_id_excitatory_main, //
+		NodeId* p_id_inhibitory_main, //
+		NodeId* p_id_rate, //
+		const typename Algorithm::Parameter& par_exc,
+		const typename Algorithm::Parameter& par_inh //
+		) {
+	MPINetwork<WeightValue, NodeDistribution> network;
+
+	// Create cortical background, and add to network
+	algorithm::RateFunctor<WeightValue> cortical_background(CorticalBackground);
+	*p_id_cortical_background = network.AddNode(cortical_background,
+			EXCITATORY);
+
+	// Create excitatory main population
+	Algorithm algorithm_excitatory(par_exc);
+	*p_id_excitatory_main = network.AddNode(algorithm_excitatory, EXCITATORY);
+
+	// Create inhibitory main population
+	Algorithm algorithm_inhibitory(par_inh);
+	*p_id_inhibitory_main = network.AddNode(algorithm_inhibitory, INHIBITORY);
+
+	// Background and excitatory connection only differ in x, 1- x
+	WeightValue connection_J_EE_BG(
+			TWOPOPULATION_C_E * (1 - TWOPOPULATION_FRACTION),
+			TWOPOPULATION_J_EE);
+
+	network.MakeFirstInputOfSecond(*p_id_cortical_background,
+			*p_id_excitatory_main, connection_J_EE_BG);
+
+	// Excitatory connection to itself
+
+	WeightValue connection_J_EE(TWOPOPULATION_C_E * TWOPOPULATION_FRACTION,
+			TWOPOPULATION_J_EE);
+
+	network.MakeFirstInputOfSecond(*p_id_excitatory_main, *p_id_excitatory_main,
+			connection_J_EE);
+
+	// Background connection to I
+
+	WeightValue connection_J_IE_BG(
+			static_cast<Number>(TWOPOPULATION_C_E * (1 - TWOPOPULATION_FRACTION)),
+			TWOPOPULATION_J_IE);
+
+	network.MakeFirstInputOfSecond(*p_id_cortical_background,
+			*p_id_inhibitory_main, connection_J_IE_BG);
+
+	// E to I
+	WeightValue connection_J_IE(
+			static_cast<Number>(TWOPOPULATION_C_E * TWOPOPULATION_FRACTION),
+			TWOPOPULATION_J_IE);
+
+	network.MakeFirstInputOfSecond(*p_id_excitatory_main, *p_id_inhibitory_main,
+			connection_J_IE);
+
+	// I to E
+	WeightValue connection_J_EI(TWOPOPULATION_C_I, -TWOPOPULATION_J_EI);
+
+	network.MakeFirstInputOfSecond(*p_id_inhibitory_main, *p_id_excitatory_main,
+			connection_J_EI);
+
+	// I to I
+	WeightValue connection_J_II(TWOPOPULATION_C_I, -TWOPOPULATION_J_II);
+
+	network.MakeFirstInputOfSecond(*p_id_inhibitory_main, *p_id_inhibitory_main,
+			connection_J_II);
+
+	return network;
+
+}
+
+int main(int argc, char* argv[]) {
+
+	try {
+		//FIXME
+		typedef double OrnsteinUhlenbeckConnection;
+
+		NodeId id_cortical_background;
+		NodeId id_excitatory_main;
+		NodeId id_inhibitory_main;
+		NodeId id_rate;
+		MPINetwork<OrnsteinUhlenbeckConnection, utilities::CircularDistribution> network =
+				CreateTwoPopulationNetwork<
+						PopulationAlgorithm_<OrnsteinUhlenbeckConnection>,
+						OrnsteinUhlenbeckConnection,
+						utilities::CircularDistribution>(
+						&id_cortical_background, &id_excitatory_main,
+						&id_inhibitory_main, &id_rate,
+						TWOPOPULATION_NETWORK_EXCITATORY_PARAMETER_POP,
+						TWOPOPULATION_NETWORK_INHIBITORY_PARAMETER_POP);
+
+		TWOPOP_HANDLER.AddNodeToCanvas(id_excitatory_main);
+		TWOPOP_HANDLER.AddNodeToCanvas(id_inhibitory_main);
+
+		bool b_configure = network.ConfigureSimulation(TWOPOP_PARAMETER);
+
+		if (!b_configure)
+			return false;
+
+		bool b_evolve = network.Evolve();
+
+		return b_evolve;
+		boost::timer::auto_cpu_timer te;
+		te.start();
+
+		//timed calculation
+		world.barrier();
+		te.stop();
+
+		if (world.rank() == 0) {
+
+			std::cout << "Time of Envolve methode of processor 0: \n";
+			te.report();
+		}
+
+	} catch (std::exception & e) {
+		std::cout << e.what();
+		env.abort(1);
+		return 1;
+	}
+
+	return 0;
+}
+}
---
+++ libs/MPILib/include/algorithm/RateFunctor.hpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2005 - 2009 Marc de Kamps
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software 
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//      If you use this software in work leading to a scientific publication, you should cite
+//      the 'currently valid reference', which can be found at http://miind.sourceforge.net
+
+#ifndef MPILIB_ALGORITHMS_RATEFUNCTOR_HPP_
+#define MPILIB_ALGORITHMS_RATEFUNCTOR_HPP_
+
+#include <MPILib/include/algorithm/AlgorithmInterface.hpp>
+
+namespace MPILib {
+namespace algorithm {
+
+typedef Rate (*RateFunction)(Time);
+
+inline Rate Nul(Time) {
+	return 0;
+}
+
+//! An Algorithm that encapsulates a rate as a function of time
+//!
+//! It is sometime necessary to provide a network with external inputs. These inputs are created as
+//! DynamicNode themselves. Their state is trivial and their output firing rate, given by the
+//! CurrentRate method follows a given function of time.
+template<class WeightValue>
+class RateFunctor: public AlgorithmInterface<WeightValue> {
+public:
+
+	//! Constructor must be initialized with pointer a rate function of time.
+	RateFunctor(RateFunction);
+
+	//! mandatory virtual destructor
+	virtual ~RateFunctor() {
+	}
+
+	//! Essentially just calling the encapsulated rate function. The connection iterators are
+	//! ignored and essentially just the current simulation time is set.
+	virtual void evolveNodeState(const std::vector<Rate>& nodeVector,
+			const std::vector<WeightValue>& weightVector, Time time);
+
+	//! Gives the current rate according to the original rate function
+	virtual Rate getCurrentRate() const {
+		return _function(_current_time);
+	}
+
+	//! Mandatory Grid  function, not of practical use.
+	virtual AlgorithmGrid getGrid() const;
+
+	virtual RateFunctor* Clone() const;
+
+	virtual void configure(const SimulationRunParameter&);
+
+	//! Gives the current time that the Algorithm keeps.
+	virtual Time getCurrentTime() const;
+
+private:
+
+	RateFunction _function;
+	Time _current_time;
+
+};
+// end of rateFunctor
+
+} /* namespace algorithm */
+} /* namespace MPILib */
+
+#endif // include guard MPILIB_ALGORITHMS_RATEFUNCTOR_HPP_
