43c3c2ffd8714ec921776dc96be2b435ee833b3f
David-Matthias Sichau <mail@sichau.eu>
2012-07-02 08:57:57 +0200

    More files added for population list

---
+++ libs/MPILib/include/populist/AbstractCirculantSolver.hpp
@@ -0,0 +1,202 @@
+// Copyright (c) 2005 - 2011 Marc de Kamps
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software 
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//      If you use this software in work leading to a scientific publication, you should include a reference there to
+//      the 'currently valid reference', which can be found at http://miind.sourceforge.net
+#ifndef MPILIB_POPULIST_ABSTRACTCIRCULANTSOLVER_HPP_
+#define MPILIB_POPULIST_ABSTRACTCIRCULANTSOLVER_HPP_
+
+#include <valarray>
+#include <MPILib/include/populist/LIFConvertor.hpp>
+
+
+
+namespace MPILib {
+namespace populist {
+
+	//! Both AbstractCirculantSolver and AbstractNonCirculantSolver instances sometimes can cut calculations short
+	//! by terminating the series \f$ e^{-t}\frac{t^k}{k!}$\f$, when it falls below a certain precision.
+	inline Number NumberOfSolverTerms(Time t, double precision, Number n_max){
+		double fact = 1.0;
+		double et = exp(-t);
+		for (Index i = 0; i < n_max; i++){
+			fact *= t/i;
+			if (et*fact < precision)
+				return i;
+		}
+		return n_max;
+	}
+
+	//! In the Configure method of AbstractCirculantSolver an InputSetParameter reference is passed in. This contains
+	//! both an integer interpretation of the current potential jump in terms of number of bins (e.g. _H_exc) as well
+	//! as a floating point. DiffusionZeroLeakEquations will work with an integer version, whilst now for example
+	//! SingleInputZeroLeakEquations allow probability transport from one bin to a point between two bins, which requires 
+	//! a floating point interpretation of the step size. This choice must be taken by a ZeroLeak developer. This developer must also
+	//! ensure that the CirculantMode and the NonCirculatMode are used consistently. Therefore also AbstractNonCirculantSolver uses this enum.
+	enum CirculantMode {FLOATING_POINT, INTEGER};
+
+	//! AbstractCirculantSolver
+	//! 
+	//! This class stores the integrated non-circulant density areas (the f^0) and the computed
+	//! circulant solution. How they are computed is a matter for the derived classes. There are
+	//! also references to the current input parameters and the density profile, so that a circulant
+	//! algorithm always has access to the variables. The Index method allows the PopulationgridControlller
+	//! to access the calculated circulant solution.
+	class AbstractCirculantSolver {
+
+	public:
+
+		//! default constructor, the mode can be set here, but when used in AbstractZeroLeakEquations they can and generally will overrule
+		//! the choice made in the constructor. 
+		AbstractCirculantSolver
+		(
+			CirculantMode = INTEGER,
+			double preceison = 0
+		);
+
+		//! virtual destructor, required for pure virtual base class
+		virtual ~AbstractCirculantSolver() = 0;
+
+		//! Configure the calculation with the current InputParameterSet
+		//! For running the AbstractCirculantSolver in INTEGER mode, _n_circ_exc, _n_noncirc_exc and _H_exc must be properly
+		//! defined, or the result will be undefined. See the doucmentation of LIFConvertor and InputParameterSet for information.
+		virtual bool Configure
+		(
+			valarray<double>*,
+			const InputParameterSet&
+		);
+
+		//! Only concrete CirculantSolvers know how to compute their contribution. At this stage it is assumed that
+		//! during configure the InputParameterSet is defined. The number of circulant bins, the number of non_circulant_areas.
+		//! H_exc and alpha_exc must all be defined.
+		virtual void Execute
+		(
+			Number,			//!< Current number of bins that needs to be solved, cannot be larger than number of elements in state array
+			Time,			//!< Time through which evolution needs to be carried out, this may not be related to the absolute time of the simulation
+			Time = 0        //!< Where the absolute time is necessary, it can be passed here
+		) = 0;
+
+		//! A CirculantSolver knows that sometimes not all circulant bins need to be taken into account
+		virtual Number NrCirculant() const;
+
+		//! Clone function
+		virtual AbstractCirculantSolver* Clone() const = 0;
+
+		//! Access to the circulant contribution. Tryimg to access beyond NrCirculant() - 1 is undefined
+		double operator[](Index) const;
+
+		//! The total amount of density which has ended up in the circulant bins
+		Density IntegratedFlux() const;
+
+		//! zero leak equations must sometimes interfere with solvers, for example by setting mode
+		friend class AbstractZeroLeakEquations;
+
+		//! It wil transfer the contributions calculated in the AbstractCirculantSolver, which knows nothing about the neuronal
+		//! parameters to the state array. Therefor it needs to receive the index of the current reset bin.
+		virtual void AddCirculantToState(Index);
+
+		//! This is a hint for AbstractZeroLeakEquations developers, whether in their implementation the Circulantsolver
+		//! should be called before of after the NonCirculantSolver. As an example, CirculantSolver should be called
+		//! before NonCirculantSolver, but RefractiveCirculantSolver should be called after NonCirculantSolver. In LIFZeroEquations
+		//! this hint allows generic code which delegates the decision which to call first to the CirculantSolver in question.
+		//! The RefractiveCirculant strategy is made default.
+		virtual bool BeforeNonCirculant() {return true;}
+
+		//! Some CirculantSolvers hold refractive probability, i.e. probability that is not represented in the current state.
+		//! If so, this method needs to be overloaded
+		virtual Probability RefractiveProbability() const {return 0.0;}
+
+		//! Some rebinners need to rescale the probability held in the queue after rebinning.
+		virtual void ScaleProbabilityQueue(double){}
+
+	protected:
+
+		void FillNonCirculantBins();
+
+		Number	_n_bins;				
+		Time	_tau;
+
+		//! The fill algorithms store the probability density integrated per non circulant areas in array_rho. The index runs
+		//! from the threshold backwards, i.e. the non circulant area bordering the threshold is area 0, conform Equation 20 in (de Kamps, 2006)
+
+		valarray<double>			_array_rho;			// integrated density in the non-circulant areas (f^0)
+		valarray<double>			_array_circulant;	// storage array for the calculated circulant solution
+		valarray<double>*			_p_array_state;		// pointer to the probability density array
+
+		const InputParameterSet*	_p_set;      // instantaneous value of the input parameters
+		double						_initial_integral;
+
+	private:
+
+		void FillLinear	();
+		void FillFP		();
+
+		void AddCirculantInteger(Index);
+		void AddCirculantFP(Index);
+
+		CirculantMode	_mode;
+
+	};
+
+	inline AbstractCirculantSolver::~AbstractCirculantSolver()
+	{
+	}
+
+	inline double AbstractCirculantSolver::operator [](Index index) const
+	{
+		return _array_circulant[index];
+	}
+
+	inline void AbstractCirculantSolver::AddCirculantToState(Index i_reversal)
+	{
+		if (_mode == INTEGER)
+			this->AddCirculantInteger(i_reversal);
+		else
+		{
+			assert( _p_set->_n_circ_exc == static_cast<Number>((_n_bins - i_reversal)/(_p_set->_H_exc + _p_set->_alpha_exc))+1);
+			this->AddCirculantFP(i_reversal);
+		}
+	}
+
+	inline void AbstractCirculantSolver::AddCirculantInteger(Index i_reversal)
+	{
+		valarray<double>& array_state = *_p_array_state;
+		Index i = i_reversal;
+		int n_circ = static_cast<int>(_p_set->_n_circ_exc);
+		for (int j = 0; j < n_circ; j++ ){
+			array_state[i] += _array_circulant[j];
+			i += _p_set->_H_exc;
+		}
+	}
+
+	inline void AbstractCirculantSolver::AddCirculantFP(Index i_reversal)
+	{
+		valarray<double>& array_state = *_p_array_state;
+		int n_circ = static_cast<int>(_p_set->_n_circ_exc);
+		for (int j = 0; j < n_circ-1; j++){
+			double h = (_p_set->_H_exc + _p_set->_alpha_exc)*j;
+			int H = static_cast<int>(floor(h)) + i_reversal;
+			double frac = h - floor(h);
+			array_state[H]   += (1-frac)*_array_circulant[j];
+			array_state[H+1] += frac*_array_circulant[j];
+		}
+		array_state[_n_bins-1]   += _array_circulant[n_circ-1];
+	}
+} /* namespace populist */
+} /* namespace MPILib */
+
+#endif // include guard MPILIB_POPULIST_ABSTRACTCIRCULANTSOLVER_HPP_
---
+++ libs/MPILib/include/populist/LIFConvertor.hpp
@@ -0,0 +1,150 @@
+// Copyright (c) 2005 - 2011 Marc de Kamps
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software 
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//      If you use this software in work leading to a scientific publication, you should include a reference there to
+//      the 'currently valid reference', which can be found at http://miind.sourceforge.net
+
+#ifndef MPILIB_POPULIST_LIFCONVERTOR_HPP_
+#define MPILIB_POPULIST_LIFCONVERTOR_HPP_
+
+#include <vector>
+
+#include <MPILib/include/algorithm/AlgorithmInterface.hpp>
+
+#include <MPILib/include/populist/InputParameterSet.hpp>
+#include <MPILib/include/populist/OrnsteinUhlenbeckParameter.hpp>
+#include <MPILib/include/BasicTypes.hpp>
+#include <MPILib/include/populist/MuSigma.hpp>
+#include "OrnsteinUhlenbeckConnection.h"
+
+
+namespace MPILib {
+namespace populist {
+
+	class PopulistSpecificParameter;
+	struct SpecialBins;
+	
+	//! Converts external input from other populations in parameters that are suitable for
+	//! AbstractCirculantSolver and AbstractNonCirculantSolver objects.
+
+	//! PopulationAlgorithm receives the contribution from its input populations as a list of pointer weight pairs. Via the
+	//! pointers, the input nodes can be queried for their current activation, via the weights their contribution be, well, weighted.
+	//! The list also contains hints as to whether for some inputs they should be interpreted as Gaussian white noise, in which case
+	//! they can be combined into a single input contribution in a way explaned below. Internally, it maintains an instance of
+	//! InputSetParameter, which contains the parameters required for AbstractCirculantSolver and AbstractNonCirculantSolver to operate
+	//! on
+
+	class LIFConvertor {
+	public:
+
+		typedef AbstractAlgorithm<PopulationConnection>::predecessor_iterator predecessor_iterator;
+
+		typedef MuSigma ScalarProductParameterType;
+
+		//! Constructor registers where the output results must be written, which can happen at construction of PopulationGridController
+		LIFConvertor
+		(
+			VALUE_REF_INIT
+			SpecialBins&				bins,
+			PopulationParameter&		par_pop,
+			PopulistSpecificParameter&	par_spec,
+			Potential&					delta_v,
+			Number&						n_bins
+
+		):
+		VALUE_MEMBER_INIT
+		_p_bins(&bins),
+		_p_par_pop(&par_pop),
+		_p_par_spec(&par_spec),
+		_p_delta_v(&delta_v),
+		_p_n_bins(&n_bins),
+		_b_toggle_sort(false),
+		_b_toggle_diffusion(false)
+		{
+		}
+
+		//! A signaller for when the PopulationGridController starts to configure
+		void Configure
+		(
+			valarray<Potential>&
+		);
+
+		//! This function collects the external input, and lays it out internally for use in AdaptParameters. This function must
+		//! be called when input charateristics change, for example at the start of an Evolve. It used to be part of AdaptParameters,
+		//! but has been moved out of there to help account for synchronous updating in networks. DynamicNetworkImplementation will now
+		//! first go through a loop calling nodes to update their input, and then a second loop to evolve their states.
+		void SortConnectionvector
+		(
+			predecessor_iterator,
+			predecessor_iterator
+		);
+
+
+		//! This function interprets the  input contribution from other populations and converts them into input parameters for the
+		//! circulant and non circulant solvers. It must be run every time _delta_v changes.
+		void AdaptParameters();
+
+		//! This function assumes that the current input is known and stored in the input parameter set, but that the parameters
+		//! for the circulant and non-circulant solver is wrong. One case where this happens is after rebinning: the input firing rates
+		//! and efficacies have changed, but the solver parameters are invalid. This function recalculates them.
+		void RecalculateSolverParameters
+		(
+		);
+
+		InputParameterSet& SolverParameter(){ return  _input_set; }
+
+		const InputParameterSet& SolverParameter() const { return _input_set; }
+
+		const PopulationParameter& ParPop() const { return *_p_par_pop; }
+
+		const Index& IndexReversalBin() const;
+
+		const Index& IndexCurrentResetBin() const;
+
+		void UpdateRestInputParameters();
+
+#ifdef _INVESTIGATE_ALGORITHM
+		vector<ReportValue>& Values(){ return _vec_value; }
+#endif
+	private:
+
+		bool IsSingleDiffusionProcess(Potential h) const;
+
+		void SetDiffusionParameters(const MuSigma&);
+
+		VALUE_MEMBER_REF
+
+		Time*						_p_time;
+		SpecialBins*				_p_bins;
+		InputParameterSet			_input_set;
+		PopulationParameter*		_p_par_pop;
+		PopulistSpecificParameter*	_p_par_spec;
+		Rate**						_pp_rate;
+		Potential*					_p_delta_v;
+		Number*						_p_n_bins;
+		Index*						_p_index_reversal_bin;
+		bool						_b_toggle_sort;
+		bool						_b_toggle_diffusion;
+	
+		std::vector<predecessor_iterator>			_vec_burst;
+		std::vector<predecessor_iterator>			_vec_diffusion;
+	};
+
+} /* namespace populist */
+} /* namespace MPILib */
+
+#endif //include guard MPILIB_POPULIST_LIFCONVERTOR_HPP_
