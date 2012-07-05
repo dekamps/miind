0f6928343317ab2b6abe6b87e9cebb7fe9e706e2
David-Matthias Sichau <mail@sichau.eu>
2012-07-05 11:53:53 +0200

    Added source files to cmake

--- libs/MPILib/src/populist/ABConvertor.cpp
+++ libs/MPILib/src/populist/ABConvertor.cpp
@@ -1,76 +1,57 @@
-#include "ABConvertor.h"
-#include "AbstractCirculantSolver.h"
-#include "PopulistSpecificParameter.h"
+#include <MPILib/include/populist/ABConvertor.hpp>
+#include <MPILib/include/populist/AbstractCirculantSolver.hpp>
+#include <MPILib/include/populist/PopulistSpecificParameter.hpp>
 
+namespace MPILib {
+namespace populist {
 
-using namespace PopulistLib;
-
-
-ABConvertor::ABConvertor
-(
-	VALUE_REF_INIT
-	SpecialBins&,
-	PopulationParameter&		par_pop,
-	PopulistSpecificParameter&	par_specific,
-	Potential&					delta_v,	
-	Number&						n_current_bins
-):
-VALUE_MEMBER_INIT 
-_p_specific	(&par_specific),
-_p_pop		(&par_pop),
-_p_n_bins	(&n_current_bins),
-_p_delta_v	(&delta_v)
-{
+ABConvertor::ABConvertor( VALUE_REF_INIT
+SpecialBins&, PopulationParameter& par_pop,
+		PopulistSpecificParameter& par_specific, Potential& delta_v,
+		Number& n_current_bins) :
+		VALUE_MEMBER_INIT
+		_p_specific(&par_specific), _p_pop(&par_pop), _p_n_bins(
+				&n_current_bins), _p_delta_v(&delta_v) {
 }
 
 const PopulistSpecificParameter&
-	ABConvertor::PopSpecific() const
-{
+ABConvertor::PopSpecific() const {
 	return *_p_specific;
 }
 
 const OneDMInputSetParameter&
-	ABConvertor::InputSet() const
-{
+ABConvertor::InputSet() const {
 	return _param_input;
 }
 
-void ABConvertor::SortConnectionvector
-(
-	predecessor_iterator	iter_begin,
-	predecessor_iterator	iter_end
-)
-{
-	_param_input._par_input =
-			_scalar_product.Evaluate
-			(
-				iter_begin, 
-				iter_end,
-				_p_pop->_tau	
-			);
-		_param_input._par_input._q = _param_onedm._par_adapt._q;
+void ABConvertor::SortConnectionvector(predecessor_iterator iter_begin,
+		predecessor_iterator iter_end) {
+	_param_input._par_input = _scalar_product.Evaluate(iter_begin, iter_end,
+			_p_pop->_tau);
+	_param_input._par_input._q = _param_onedm._par_adapt._q;
 }
 
-void ABConvertor::AdaptParameters
-(	
+void ABConvertor::AdaptParameters(
 
-)
-{
+) {
 
 	RecalculateSolverParameters();
 }
 
-void ABConvertor::RecalculateSolverParameters()
-{
+void ABConvertor::RecalculateSolverParameters() {
 	_param_input._n_current_bins = *_p_n_bins;
-	_param_input._n_max_bins     = _p_specific->MaxNumGridPoints();
+	_param_input._n_max_bins = _p_specific->MaxNumGridPoints();
 
 	// current expansion factor is current number of bins
 	// divided by number of initial bins
 
-	double f = static_cast<double>(_param_input._n_current_bins)/static_cast<double>(_p_specific->NrGridInitial());
-	_param_input._q_expanded = f*_param_input._par_input._q;
-	_param_input._t_since_rebinning = _p_pop->_tau*log(f);
+	double f = static_cast<double>(_param_input._n_current_bins)
+			/ static_cast<double>(_p_specific->NrGridInitial());
+	_param_input._q_expanded = f * _param_input._par_input._q;
+	_param_input._t_since_rebinning = _p_pop->_tau * log(f);
 	_param_input._g_max = _p_pop->_theta;
-	_param_input._tau   = _p_pop->_tau;
+	_param_input._tau = _p_pop->_tau;
 }
+
+} /* namespace populist */
+} /* namespace MPILib */
