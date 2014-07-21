7dee5a8bb164061b209d603f9d72ec367471092c
Marc de Kamps <m.dekamps@leeds.ac.uk>
2014-07-21 18:06:43 +0100

    modified:   Mainpage.hpp

--- doc/Mainpage.hpp
+++ doc/Mainpage.hpp
@@ -1,13 +1,22 @@
 /*!
 \mainpage Miind-mpi Documentation
 \section sec_intro Introduction
-The motivation for \a MIIND is described in the \a MIIND white paper \cite{dekamps2006c}. Basically \a MIIND aims
-to provide libaries for simulations in cognition that very small but useful, so that users are tempted to incorporate
-them into their own projects. It explicitly tries to avoid forcing a modelling methodology on the user. It aims to offer
-components that the user would want to write anyway, without offering much else. The components are supposed to be so
-mature that they don't require many extensions anymore. Until there is an adopted standard for cognitive modelling and an
-infrastructure in place for developping and maintaining software in this area, this is probably the only way to tempt
-modellers into software reuse.
+MIIND provides a simulation framework for neural simulations. It focusses on population level
+descriptions of neural dynamics, i.e. it does not provide a siumulator for individual neurons,
+but models populations directly. It provides support for simple neural mass models, but
+focusses strongly on so-called population density techniques.
+
+To get a feeling for the simulator and its capabilities,
+we encourage you to read the following <a href="http://www.sciencedirect.com/science/article/pii/S0893608008001457">paper</a> 
+(an open access <a href="http://eprints.whiterose.ac.uk/5235/">preprint</a> version is also available). 
+
+\image html hbp.png
+
+Currently, part of the developent is funded by the <a href="http://www.humanbrainproject.eu/">Human Brain Project</a>.  
+A new official release date will be 1 October 2014. The new release will contain many new algorithms, including
+other populations than leaky-integrate-and-fire ones. The documentation will be revamped.
+
+
 
 The components are Open Source software under a BSD licence.
 \section licence Licence
