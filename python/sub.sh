06efc31ea4faad8aa5e6492df3621d13a1a9a512
Marc de Kamps <scsmdk@csltp086.lan>
2015-05-12 17:06:02 +0100

    new file:   sub.sh

---
+++ python/sub.sh
@@ -0,0 +1,12 @@
+# Example script: sample_script.sh
+#!/bin/bash
+# Set current working directory
+#$ -cwd
+#Use current environment variables/ modules
+#$ -V
+#Request one hour of runtime
+#$ -l h_rt=1:00:00
+#Email at the beginning and end of the job
+#$ -m be
+#Run the executable 'myprogram' from the current working directory
+$1
