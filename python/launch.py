55449fd5cc4aa979d212e67ca741e07ce2758883
Marc de Kamps <scsmdk@csltp086.lan>
2015-05-12 20:14:45 +0100

    modified:   launch.py

--- python/launch.py
+++ python/launch.py
@@ -1,7 +1,20 @@
 import subprocess 
+import submit
 
-f=open('joblist')
-lines=f.readlines()
+
+dir=sys.argv[1]
+
+build = os.path.join(directories.miind_root(),'build')
+path  = os.path.join(build,'jobs',dir)
+
+with submit.cd(build):
+        subprocess.call(["ls","-l"])
+        subprocess.call(['make'])
+
+with submit.cd(build):
+	f=open('joblist')
+	lines=f.readlines()
+
 for line in lines:
 	name=line.strip()
 	subprocess.call(['qsub','./sub.sh',name])
