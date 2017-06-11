94b1f149238bae73df8bc3cf0992bd3fac7c2f6b
Marc de Kamps <scsmdk@leeds.ac.uk>
2017-06-11 14:25:06 +0100

    modified:   demo2D.py

--- python/demo2D.py
+++ python/demo2D.py
@@ -1,25 +1,33 @@
+#!/usr/bin/env python
+
+import ROOT
 import os
 import visualize
 import sys
 import time
 import subprocess
  
-MONITOR=os.path.dirname(os.path.realpath(__file__)) 
+MONITOR=os.path.basename(os.getcwd())
 
 def processlist(modelname):
-    filelist=subprocess.check_output(["ls",MONITOR + '/' + modelname + '_mesh']).split()
+    filelist=subprocess.check_output(["ls", modelname + '_mesh']).split()
     sortlist=sorted(filelist, key=lambda item: (float(item.split('_')[-2]) ))
     f=open('processlist.txt','w')
     for fi in sortlist:
         f.write('file \'' + fi + '.png\'\n' )
 
-def loop(modelname,option):
-    m=visualize.ModelVisualizer(modelname)
+def loop(modelname,option,batch=False):
 
+    if batch == True:
+        print 'switching to batch'	
+        ROOT.gROOT.SetBatch(True) 
+
+    m=visualize.ModelVisualizer(modelname)
     i = 0
+
     while(1):
     
-        filelist=subprocess.check_output(["ls",MONITOR + '/' + modelname + '_mesh']).split()
+        filelist=subprocess.check_output(["ls", modelname + '_mesh']).split()
         # nice, but filelist is ordered alphabetically. want sort on time which we expect (!) is the last field
         sortlist=sorted(filelist, key=lambda item: (float(item.split('_')[-2]) ))
         print sortlist
@@ -28,14 +36,16 @@ def loop(modelname,option):
             timestring = sortlist[i].split('_')[-2]
             print sortlist[i], timestring
             if (option == ''):
-                m.showfile(MONITOR + '/' + modelname + '_mesh' + '/' + sortlist[i], runningtext = 't = ' + timestring)
+                m.showfile( modelname + '_mesh' + '/' + sortlist[i], runningtext = 't = ' + timestring)
             else:
-                m.showfile(MONITOR + '/' + modelname + '_mesh' + '/' + sortlist[i], pdfname=sortlist[i], runningtext = 't = ' + timestring)
+                m.showfile( modelname + '_mesh' + '/' + sortlist[i], pdfname=sortlist[i], runningtext = 't = ' + timestring)
                 
             i+=1
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         loop(sys.argv[1],'')
-    else:
+    if len(sys.argv) == 3:
         loop(sys.argv[1],'y')
+    if len(sys.argv) == 4:
+	loop(sys.argv[1],'y',True)
