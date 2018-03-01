b22e6e1a1568e7b340a6bedb0037650162bae19c
Hugh Osborne <hugh.osborne@gmail.com>
2018-03-01 21:16:46 +0000

    Introduce MiindIO

---
+++ python/api/density.py
@@ -0,0 +1,287 @@
+import os.path as op
+from tools import *
+import mesh as meshmod
+import glob
+import subprocess
+import time
+import matplotlib.pyplot as plt
+from collections import OrderedDict as odict
+from shapely.geometry import Polygon
+from descartes.patch import PolygonPatch
+from matplotlib.collections import PatchCollection
+
+
+def replace(value, string, *args):
+    for a in args:
+        value = value.replace(a, string)
+    return value
+
+
+def find_density_fnames(modelfname, directory):
+    fnames = glob.glob(op.join(directory, modelfname + '_mesh', 'mesh*'))
+    if len(fnames) == 0:
+        raise ValueError('No density output found for {}'.format(modelfname))
+
+    fnames = sorted(fnames, key=get_density_time)
+    times = [get_density_time(f) for f in fnames]
+    return fnames, times
+
+
+def read_density(filename):
+    f = open(filename, 'r')
+    line = f.readline().split()
+    data = [float(x) for x in line[2::3]]
+    coords = [(int(i), int(j)) for i, j in zip(line[::3], line[1::3])]
+    return data, coords
+
+
+def get_density_time(path):
+    fname = op.split(path)[-1]
+    return float(fname.split('_')[2])
+
+
+def calc_mass(mesh, density, coords):
+    masses = [mesh.cells[i][j].area * dens
+              for (i, j), dens in zip(coords, density)]
+    return masses
+
+
+class General(object):
+    def __init__(self, io, modelname):
+        self.io = io
+        self.modelname, self.modelfname = split_fname(modelname, '.model')
+        self.modelpath = op.join(self.io.xml_location, self.modelfname)
+
+    @property
+    def fnames(self):
+        if not hasattr(self, '_fnames'):
+            self._fnames, self._times = find_density_fnames(
+                self.modelfname, self.io.output_directory)
+        return self._fnames
+
+    @property
+    def times(self):
+        if not hasattr(self, '_times'):
+            self.fnames
+        return self._times
+
+    @property
+    def mesh(self):
+        if not hasattr(self, '_mesh'):
+            mesh = meshmod.Mesh(None)
+            mesh.FromXML(self.modelpath)
+            self._mesh = mesh
+        return self._mesh
+
+
+class Marginal(General):
+    def __init__(self, io, modelname, vn=100, wn=100):
+        super(Marginal, self).__init__(io, modelname)
+        self.path = op.join(self.io.output_directory,
+                            self.modelname + '_marginal_density')
+        self.data_path = op.join(self.io.output_directory, 'marginal_density.npz')
+        self.projfname = self.modelpath.replace('.model', '.projection')
+        self.vn, self.wn = vn, wn
+
+    def __getitem__(self, name):
+        return self.density[name]
+
+    @property
+    def density(self):
+        self.read_projection()
+        if op.exists(self.data_path) and not self.new_projection:
+            load_data = np.load(self.data_path)['data'][()]
+            if self.modelname in load_data:
+                return load_data[self.modelname]
+        v = np.zeros((len(self.fnames), self.projection['N_V']))
+        w = np.zeros((len(self.fnames), self.projection['N_W']))
+        masses, coords_ = [], None
+        for ii, fname in enumerate(self.fnames):
+            density, coords = read_density(fname)
+            if coords_ is None:
+                coords_ = coords
+            else:
+                assert coords == coords_
+            masses.append(calc_mass(self.mesh, density, coords))
+        masses = np.vstack(masses)
+        assert masses.shape[0] == len(self.fnames)
+        v, w, bins_v, bins_w = self.calc_marginal_density(
+            v, w, masses, coords, self.projection)
+        data = {'v': v, 'w': w, 'bins_v': bins_v,
+                'bins_w': bins_w, 'times': self.times}
+        if op.exists(self.data_path):
+            other = np.load(self.data_path)['data'][()]
+            other.update({self.modelname: data})
+            save_data = other
+        else:
+            save_data = {self.modelname: data}
+        np.savez(self.data_path, data=save_data)
+        return data
+
+    def calc_marginal_density(self, v, w, masses, coords, proj):
+
+        def scale(var, proj, mass):
+            bins = [marg.split(',') for marg in proj.split(';')
+                    if len(marg) > 0]
+            for jj, dd in bins:
+                var[:, int(jj)] += mass * float(dd)
+            return var
+        for trans in proj['transitions']:
+            i, j = [int(a) for a in trans['coordinates'].split(',')]
+            cell_mass = masses[:, coords.index((i, j))]
+            if np.all(cell_mass < 1e-15):
+                continue
+            v = scale(v, trans['vbins'], cell_mass)
+            w = scale(w, trans['wbins'], cell_mass)
+        dv = abs(proj['V_max'] - proj['V_min']) / float(proj['N_V'])
+        dw = abs(proj['W_max'] - proj['W_min']) / float(proj['N_W'])
+        for idx in range(v.shape[0]):
+            v[idx] = v[idx] / dv / v[idx].sum()
+            w[idx] = w[idx] / dw / w[idx].sum()
+        bins_v = np.linspace(proj['V_min'], proj['V_max'], proj['N_V'])
+        bins_w = np.linspace(proj['W_min'], proj['W_max'], proj['N_W'])
+        return v, w, bins_v, bins_w
+
+    def make_projection_file(self):
+        projection_exe = op.join(self.io.MIIND_APPS, 'Projection', 'Projection')
+        out = subprocess.check_output(
+          [projection_exe, self.modelfname], cwd=self.io.xml_location)
+        vmax, wmax = np.array(out.split('\n')[3].split(' ')[2:], dtype=float)
+        vmin, wmin = np.array(out.split('\n')[4].split(' ')[2:], dtype=float)
+        # assert that we bound the range
+        inc = lambda x: x * 1.01 if x > 0 else x * 0.99
+        vmax, wmax = inc(vmax), inc(wmax)
+        vmin, wmin = -inc(-vmin) , -inc(-wmin)
+        cmd = [projection_exe, self.modelfname, vmin, vmax,
+               self.vn, wmin, wmax, self.wn]
+        subprocess.call([str(c) for c in cmd], cwd=self.io.xml_location)
+
+    def read_projection(self):
+        proj_pathname = op.join(self.io.xml_location, self.projfname)
+        self.new_projection = False
+        if not op.exists(proj_pathname):
+            print('No projection file found, generating...')
+            self.make_projection_file()
+            self.new_projection = True
+        if not hasattr(self, 'projection'):
+            self._proj = xml_to_dict(ET.parse(proj_pathname).getroot(),
+                                      text_content=None)['Projection']
+        if (self._proj['W_limit']['N_W'] != self.wn or
+            self._proj['V_limit']['N_V'] != self.vn):
+            print('New N in bins, generating projection file...')
+            self.make_projection_file()
+            self._proj = xml_to_dict(ET.parse(proj_pathname).getroot(),
+                                      text_content=None)['Projection']
+            self.new_projection = True
+        self.projection =  {
+            'transitions': self._proj['transitions']['cell'],
+            'V_min': self._proj['V_limit']['V_min'],
+            'V_max': self._proj['V_limit']['V_max'],
+            'N_V': self._proj['V_limit']['N_V'],
+            'W_min': self._proj['W_limit']['W_min'],
+            'W_max': self._proj['W_limit']['W_max'],
+            'N_W': self._proj['W_limit']['N_W'],
+        }
+
+    def plot(self):
+        if not op.exists(self.path):
+            os.mkdir(self.path)
+        for ii in range(len(self['times'])):
+            fig, axs = plt.subplots(1, 2)
+            params = {
+                'ax': axs,
+                'dens': [self['v'], self['w']],
+                'bins': [self['bins_v'], self['bins_w']]
+            }
+            params = [{k: v[i] for k, v in params.items()}
+                      for i in range(len(params['ax']))]
+            plt.suptitle('time = {}'.format(self['times'][ii]))
+            for p in params:
+                p['ax'].plot(p['bins'], p['dens'][ii, :])
+                figname = op.join(self.path,
+                                  '{}_'.format(ii) +
+                                  '{}.png'.format(self['times'][ii]))
+                fig.savefig(figname, res=300, bbox_inches='tight')
+                plt.close(fig)
+
+
+class Density(General):
+    def __init__(self, io, modelname):
+        super(Density, self).__init__(io, modelname)
+        self.path = op.join(self.io.output_directory,
+                            self.modelname + '_density')
+
+    @property
+    def polygons(self):
+        if not hasattr(self, '_polygons'):
+            self._polygons = odict(
+                ((i, j),
+                Polygon([(float(x), float(y))
+                         for x, y in zip(cell.vs, cell.ws)]))
+                for i, cells in enumerate(self.mesh.cells)
+                for j, cell in enumerate(cells)
+            )
+        return self._polygons
+
+    @property
+    def patches(self):
+        if not hasattr(self, '_patches'):
+            self._patches = [
+                PolygonPatch(polygon)
+                for polygon in self.polygons.values()
+            ]
+        return self._patches
+
+    def plot_mesh(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots()
+        md = self.mesh.dimensions()
+        p = PatchCollection(self.patches, alpha=1, edgecolors='k',
+                            facecolors='w')
+        ax.add_collection(p)
+        ax.set_xlim(md[0])
+        ax.set_ylim(md[1])
+        aspect = (md[0][1] - md[0][0]) / (md[1][1] - md[1][0])
+        ax.set_aspect(aspect)
+        return ax
+
+    def colscale(self, density):
+        cols = np.log10(np.array(density) + 1e-6)
+        vmax = np.max(cols)
+        vmin = np.min(cols)
+        vals = (cols - vmin)/(vmax - vmin)
+        return vmin, vmax, vals
+
+    def plot_density(self, fname, colorbar=None, cmap='inferno', ax=None,
+                     save=False, ext='.png'):
+        if not ext.startswith('.'):
+            ext = '.' + ext
+        if ax is None:
+            fig, ax = plt.subplots()
+        time = get_density_time(fname)
+        idx = self.times.index(time)
+        md = self.mesh.dimensions()
+        poly_coords = list(self.polygons.keys())
+        ax.set_xlim(md[0])
+        ax.set_ylim(md[1])
+        aspect = (md[0][1] - md[0][0]) / (md[1][1] - md[1][0])
+        ax.set_aspect(aspect)
+        p = PatchCollection(self.patches, cmap=cmap)
+        density, coords = read_density(fname)
+        sort_idx = sorted(range(len(coords)), key=coords.__getitem__)
+        coords = [coords[i] for i in sort_idx]
+        density = [density[i] for i in sort_idx]
+        assert coords == poly_coords
+        vmin, vmax, scaled_density = self.colscale(density)
+        p.set_array(scaled_density)
+        ax.add_collection(p)
+        if colorbar is not None:
+            plt.colorbar(p)
+        if save:
+            if not op.exists(self.path):
+                os.mkdir(self.path)
+            figname = op.join(
+                self.path, '{}_'.format(idx) +
+                '{}'.format(time)).replace('.', '-')
+            plt.gcf().savefig(figname + ext, res=300, bbox_inches='tight')
+            plt.close(plt.gcf())
---
+++ python/api/lost_tools.py
@@ -0,0 +1,143 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+import ast
+import xml.etree.ElementTree as ET
+
+
+def write_fid(fid_fname, quadlist):
+    with open(fid_fname, 'w') as f:
+        f.write('<Fiducial>\n')
+        for quad in quadlist:
+
+            vs = [ point[0] for point in quad ]
+            ws = [ point[1] for point in quad ]
+
+            f.write('<Quadrilateral type= \"Contain\">')
+            f.write('<vline>')
+            for v in vs:
+                f.write(str(v) + ' ')
+            f.write('</vline>')
+            f.write('<wline>')
+            for w in ws:
+                f.write(str(w) + ' ')
+            f.write('</wline>')
+            f.write('</Quadrilateral>\n')
+        f.write('</Fiducial>\n')
+
+
+def read_file(fn):
+    f = open(fn)
+    lines = f.readlines()
+    x = [float(item.split()[0]) for item in lines]
+    y = [float(item.split()[1]) for item in lines]
+    return x, y
+
+
+def plot_lost(fn):
+    ax = plt.gcf().add_subplot(111)
+    x, y = read_file(fn)
+    plt.plot(x, y, '.')
+    return ax
+
+
+def add_fiducial(ax, point_list):
+    from matplotlib.path import Path
+    verts = [(x[0], x[1]) for x in point_list]
+    verts.append((point_list[0][0], point_list[0][1]))
+
+    codes = [Path.MOVETO]
+    for x in range(1, len(point_list)):
+        codes.append(Path.LINETO)
+    codes.append(Path.CLOSEPOLY)
+
+    path  = Path(verts, codes)
+    patch = matplotlib.patches.PathPatch(path, color='orange')
+    ax.add_patch(patch)
+
+
+def read_fiducial(fn):
+    patches = []
+    tree = ET.parse(fn)
+    root = tree.getroot()
+    for child in root:
+        if child.tag != "Quadrilateral":
+            raise ValueError
+        babys = [baby for baby in child]
+
+        vs = [ float(x) for x in  babys[0].text.split() ]
+        ws = [ float(x) for x in  babys[1].text.split() ]
+
+        points = zip(vs,ws)
+        patches.append(points)
+    return patches
+
+
+def extract_base(fn):
+    return fn.replace('.lost', '').split('_')[-6]
+
+
+def zoom_fun(event, ax, base_scale=1.5):
+    # get the current x and y limits
+    cur_xlim = ax.get_xlim()
+    cur_ylim = ax.get_ylim()
+    cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
+    cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
+    xdata = event.xdata # get event x location
+    ydata = event.ydata # get event y location
+    if event.button == 'up':
+        # deal with zoom in
+        scale_factor = 1/base_scale
+    elif event.button == 'down':
+        # deal with zoom out
+        scale_factor = base_scale
+    else:
+        # deal with something that should never happen
+        scale_factor = 1
+        print event.button
+    # set new limits
+    ax.set_xlim([xdata - cur_xrange*scale_factor,
+                 xdata + cur_xrange*scale_factor])
+    ax.set_ylim([ydata - cur_yrange*scale_factor,
+                 ydata + cur_yrange*scale_factor])
+    plt.draw() # force re-draw
+
+
+def onclick(event, ax, fid_fname, curr_points, quads):
+    if event.dblclick:
+        write_fid(fid_fname, quads)
+        raise SystemExit
+
+    inv = ax.transData.inverted()
+    coords = inv.transform((event.x, event.y))
+    if len(curr_points) < 4:
+        curr_points.append(coords)
+        plt.plot(coords[0], coords[1], 'r+')
+        plt.draw()
+    else:
+        del curr_points[:]
+        curr_points.append(coords)
+        plt.plot(coords[0], coords[1], 'r+')
+        plt.draw()
+    if len(curr_points) == 4:
+        patch = [point for point in curr_points]
+        quads.append(patch)
+        add_fiducial(ax, patch)
+
+
+def onkey(event, ax, fid_fname, quads):
+    if event.key == 'd':
+        print('Deleting previous Fiducial.')
+        del quads[-1]
+        write_fid(fid_fname, quads)
+        ax.patches[-1].remove()
+        plt.draw()
+    elif event.key == 'c':
+        print('Clearing canvas and redrawing fiducials.')
+        plt.cla()
+        for patch in read_fiducial(fid_fname):
+            add_fiducial(ax, patch)
+        plt.draw()
+    else:
+        print('Key "{}" not recognized.'.format(event.key))
+        return
