7b77a97236d9736e527322c5d78f459cff1cddcf
Hugh Osborne [sc16ho] <sc16ho@cslin100.leeds.ac.uk>
2018-03-02 16:51:45 +0000

    Slightly more nuanced functionality for Variable in xml plus cleaner miind_api

--- python/variables.py
+++ python/variables.py
@@ -5,5 +5,19 @@ def parse_variables(variable_list,outfile):
     for variable in variable_list:
         name  = variable.attrib['Name']
         value = variable.text
-        s += '\tconst double ' + name + ' = ' + value +';\n'
-        outfile.write(s)
+
+        val_type = None
+        if 'Type' in variable.attrib :
+            val_type = variable.attrib['Type']
+        # Allow the user to define a C++-style data type
+        if val_type != None :
+            s += 'const ' + val_type + ' ' + name + ' = ' + value + ';\n'
+        # Otherwise, attempt to auto-detect the type here
+        # Even if a number doesn't have a decimal point, assume it's a double
+        elif value.replace('.','',1).isdigit() :
+            s += 'const double ' + name + ' = ' + value +';\n'
+        # Otherwise, assume it's a string
+        else :
+            s += 'const std::string ' + name + ' = \"' + value +'\";\n'
+    s += '\n'
+    outfile.write(s)
