5ff678b4e6edf9c899a43038f6794509a54dc0c5
Hugh Osborne [sc16ho] <sc16ho@cslin100.leeds.ac.uk>
2018-05-24 10:54:26 +0100

    Added docker stuff and removed monte carlo backup from quad overlap calculation in MatGen

---
+++ package/debian/miind.default.ex
@@ -0,0 +1,10 @@
+# Defaults for miind initscript
+# sourced by /etc/init.d/miind
+# installed at /etc/default/miind by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
