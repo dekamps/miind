dfcdd42f31edd33da68d40d11e44461533ab3a50
Marc de Kamps <scsmdk@leeds.ac.uk>
2018-07-02 16:51:09 +0100

    new file:   ../../libs/TwoDLib/Ode2DSystemGroup.cpp 	new file:   ../../libs/TwoDLib/Ode2DSystemGroup.hpp

---
+++ libs/TwoDLib/Ode2DSystemGroup.cpp
@@ -0,0 +1,272 @@
+// Copyright (c) 2005 - 2015 Marc de Kamps
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+//      and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//      If you use this software in work leading to a scientific publication, you should include a reference there to
+//      the 'currently valid reference', which can be found at http://miind.sourceforge.net
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include "Ode2DSystemGroup.hpp"
+#include "TwoDLibException.hpp"
+
+
+using namespace TwoDLib;
+
+
+std::vector<Ode2DSystemGroup::Clean> Ode2DSystemGroup::InitializeClean()
+{
+	std::vector<Ode2DSystemGroup::Clean> vec_ret;
+	for (MPILib::Index m = 0; m < _mesh_list.size(); m++){
+		Clean clean(*this,_vec_mass,m);
+		vec_ret.push_back(clean);
+	}
+	return vec_ret;
+}
+
+std::vector<Ode2DSystemGroup::Reset> Ode2DSystemGroup::InitializeReset()
+{
+	std::vector<Ode2DSystemGroup::Reset> vec_ret;
+	for (MPILib::Index m = 0; m < _mesh_list.size(); m++){
+		Reset reset(*this,_vec_mass,m);
+		vec_ret.push_back(reset);
+	}
+	return vec_ret;
+}
+
+std::vector<Ode2DSystemGroup::Reversal> Ode2DSystemGroup::InitializeReversal()
+{
+	std::vector<Ode2DSystemGroup::Reversal> vec_ret;
+	for (MPILib::Index m = 0; m < _mesh_list.size(); m++){
+		Reversal reversal(*this,_vec_mass,m);
+		vec_ret.push_back(reversal);
+	}
+	return vec_ret;
+}
+
+Ode2DSystemGroup::Ode2DSystemGroup
+(
+	const std::vector<Mesh>& mesh_list,
+	const std::vector<std::vector<Redistribution> >& vec_reversal,
+	const std::vector<std::vector<Redistribution> >& vec_reset
+):
+_mesh_list(mesh_list),
+_vec_mesh_offset(MeshOffset(mesh_list)),
+_vec_length(InitializeLengths(mesh_list)),
+_vec_cumulative(InitializeCumulatives(mesh_list)),
+_vec_mass(InitializeMass()),
+_vec_area(InitializeArea(mesh_list)),
+_t(0),
+_fs(std::vector<double>(mesh_list.size(),0.0)),
+_avs(std::vector<double>(mesh_list.size(),0.0)),
+_map(InitializeMap()),
+_linear_map(InitializeLinearMap()),
+_vec_reversal(vec_reversal),
+_vec_reset(vec_reset),
+_reversal(InitializeReversal()),
+_reset(InitializeReset()),
+_clean(InitializeClean())
+{
+	assert(m.TimeStep() != 0.0);
+//	this->CheckConsistency();
+}
+
+std::vector<MPILib::Number> Ode2DSystemGroup::MeshOffset(const std::vector<Mesh>& l) const
+{
+	std::vector<MPILib::Number> vec_ret{0}; // first offset is 0
+	for (const Mesh& m: l){
+		MPILib::Number n_cell = 0;
+		for (MPILib::Index i = 0; i < m.NrQuadrilateralStrips(); i++)
+			for( MPILib::Index j = 0; j < m.NrCellsInStrip(i); j++)
+				n_cell++;
+		vec_ret.push_back(n_cell);
+	}
+
+	return vec_ret;
+}
+
+
+std::vector<MPILib::Index> Ode2DSystemGroup::InitializeCumulative(const Mesh& m) const
+{
+	unsigned int sum = 0;
+	vector<unsigned int> vec_ret;
+	vec_ret.push_back(0);
+	for(unsigned int i = 0; i < m.NrQuadrilateralStrips(); i++){
+		sum += m.NrCellsInStrip(i);
+		vec_ret.push_back(sum);
+	}
+	return vec_ret;
+}
+
+std::vector<std::vector<MPILib::Index> > Ode2DSystemGroup::InitializeCumulatives(const std::vector<Mesh>& mesh_list)
+{
+	std::vector<std::vector<MPILib::Index> > vec_ret;
+	for(const Mesh& m: mesh_list)
+		vec_ret.push_back(this->InitializeCumulative(m));
+	return vec_ret;
+}
+
+vector<double> Ode2DSystemGroup::InitializeMass() const
+{
+	MPILib::Number n_cells = 0;
+	for(const std::vector<MPILib::Index>& v: _vec_cumulative)
+		n_cells += v.back();
+
+	return vector<double>(n_cells,0.0);
+}
+
+std::vector<MPILib::Index> Ode2DSystemGroup::InitializeLength(const Mesh& m) const
+{
+	std::vector<MPILib::Index> vec_ret;
+	for(unsigned int i = 0; i < m.NrQuadrilateralStrips();i++)
+		vec_ret.push_back(m.NrCellsInStrip(i));
+	return vec_ret;
+}
+
+std::vector< std::vector<MPILib::Index> > Ode2DSystemGroup::InitializeLengths(const std::vector<Mesh>& list)
+{
+	std::vector< std::vector<MPILib::Index> > vec_ret;
+	for(const Mesh& m: list)
+		vec_ret.push_back(this->InitializeLength(m));
+
+	return vec_ret;
+}
+
+
+vector<double> Ode2DSystemGroup::InitializeArea(const std::vector<Mesh>& vec) const
+{
+	vector<double> vec_ret;
+	for (const Mesh& m: _mesh_list){
+		for (unsigned int i = 0; i < m.NrQuadrilateralStrips(); i++)
+			for (unsigned int j = 0; j < m.NrCellsInStrip(i); j++ )
+				vec_ret.push_back(m.Quad(i,j).SignedArea());
+	}
+	return vec_ret;
+}
+
+void Ode2DSystemGroup::Initialize(MPILib::Index m, MPILib::Index i, MPILib::Index j){
+	_vec_mass[this->Map(m,i,j)] = 1.0;
+}
+
+std::vector< std::vector< std::vector<MPILib::Index> > > Ode2DSystemGroup::InitializeMap() const
+{
+	std::vector< std::vector<std::vector<MPILib::Index> > > vec_map;
+	MPILib::Index count = 0;
+
+	for(const Mesh& mesh: _mesh_list){
+		std::vector<std::vector<MPILib::Index> > vec_mesh;
+		for (MPILib::Index i = 0; i < mesh.NrQuadrilateralStrips();i++){
+			std::vector<MPILib::Index> vec_strip;
+			for (MPILib::Index j = 0; j < mesh.NrCellsInStrip(i); j++){
+				vec_strip.push_back(count++);
+			}
+			vec_mesh.push_back(vec_strip);
+		}
+		vec_map.push_back(vec_mesh);
+	}
+	return vec_map;
+}
+
+std::vector<MPILib::Index> Ode2DSystemGroup::InitializeLinearMap()
+{
+	std::vector<MPILib::Index> vec_ret;
+	MPILib::Index counter = 0;
+	for( const Mesh& mesh: _mesh_list){
+		for (MPILib::Index i = 0; i < mesh.NrQuadrilateralStrips(); i++)
+			for (MPILib::Index j = 0; j < mesh.NrCellsInStrip(i); j++)
+				vec_ret.push_back(counter++);
+	}
+	return vec_ret;
+}
+
+void Ode2DSystemGroup::Dump(const std::vector<std::ostream*>& vecost, int mode) const
+{
+	assert(vecost.size() == _mesh_list.size());
+	for(MPILib::Index m = 0; m < _mesh_list.size(); m++){
+		vecost[m]->precision(10);
+		if (mode == 0) {
+			for (unsigned int i = 0; i < _mesh_list[m].NrQuadrilateralStrips(); i++)
+				for (unsigned int j = 0; j < _mesh_list[m].NrCellsInStrip(i); j++ )
+					// a division by _vec_area[this->Map(i,j)] is wrong
+					// the fabs is required since we don't care about the sign of the area and
+					// must write out a positive density
+					(*vecost[m]) << i << "\t" << j << "\t" << " " << fabs(_vec_mass[this->Map(m,i,j)]/_mesh_list[m].Quad(i,j).SignedArea()) << "\t";
+		} else {
+			for (unsigned int i = 0; i < _mesh_list[m].NrQuadrilateralStrips(); i++)
+				for (unsigned int j = 0; j < _mesh_list[m].NrCellsInStrip(i); j++ )
+					(*vecost[m]) << i << "\t" << j << "\t" << " " << _vec_mass[this->Map(m,i,j)] << "\t";
+		}
+	}
+}
+
+void Ode2DSystemGroup::Evolve()
+{
+	_t += 1;
+	for (double& f: _fs)
+		f = 0.;
+	this->UpdateMap();
+}
+
+void Ode2DSystemGroup::UpdateMap()
+{
+	MPILib::Index counter = 0;
+	for (MPILib::Index m = 0; m < _mesh_list.size(); m++){ // we need the index for mapping, so no range-based loop
+		for(MPILib::Index i_stat = 0; i_stat < _mesh_list[m].NrCellsInStrip(0); i_stat++)
+			_linear_map[counter++] = i_stat + _vec_mesh_offset[m]; // the stationary strip needs to be handled separately
+		for (MPILib::Index i = 1; i < _mesh_list[m].NrQuadrilateralStrips(); i++){
+			// yes! i = 1. strip 0 is not supposed to have dynamics
+			for (MPILib::Index j = 0; j < _mesh_list[m].NrCellsInStrip(i); j++ ){
+				MPILib::Index ind = _vec_cumulative[m][i] + modulo(j-_t,_vec_length[m][i]) + _vec_mesh_offset[m];
+				_map[m][i][j] = ind;
+				_linear_map[counter++] = ind;
+			}
+		}
+	}
+}
+
+void Ode2DSystemGroup::RemapReversal(){
+	for(MPILib::Index m = 0; m < _mesh_list.size(); m++)
+		std::for_each(_vec_reversal[m].begin(),_vec_reversal[m].end(),_reversal[m]);
+}
+
+void Ode2DSystemGroup::RedistributeProbability()
+{
+	for(MPILib::Index m=0; m < _mesh_list.size(); m++){
+		std::for_each(_vec_reset[m].begin(),_vec_reset[m].end(),_reset[m]);
+		std::for_each(_vec_reset[m].begin(),_vec_reset[m].end(),_clean[m]);
+	}
+	double t_step = _mesh_list[0].TimeStep(); // they all should have the same time step
+	for (double& f: _fs)
+		f /= t_step;
+}
+
+const std::vector<double>& Ode2DSystemGroup::AvgV() const
+{
+	// Rate calculation for non-threshold crossing models such as Fitzhugh-Nagumo
+	for(MPILib::Index m = 0; m < _mesh_list.size(); m++){
+		double av = 0.;
+		for(MPILib::Index i = 0; i < _mesh_list[m].NrQuadrilateralStrips(); i++){
+			for(MPILib::Index j = 0; j < _mesh_list[m].NrCellsInStrip(i); j++){
+				double V = _mesh_list[m].Quad(i,j).Centroid()[0];
+				av += V*_vec_mass[this->Map(m,i,j)];
+			}
+		}
+		const_cast<double&>(_avs[m]) = av;
+	}
+	return _avs;
+}
+
---
+++ libs/TwoDLib/Ode2DSystemGroup.hpp
@@ -0,0 +1,209 @@
+// Copyright (c) 2005 - 2015 Marc de Kamps
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+//      and/or other materials provided with the distribution.
+//    * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+//      without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//      If you use this software in work leading to a scientific publication, you should include a reference there to
+//      the 'currently valid reference', which can be found at http://miind.sourceforge.net
+
+#ifndef _CODE_2DLIB_ODE2DSYSTEMGROUP_INCLUDE_GUARD
+#define _CODE_2DLIB_ODE2DSYSTEMGROUP_INCLUDE_GUARD
+
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include "MPILib/include/TypeDefinitions.hpp"
+#include "Mesh.hpp"
+#include "modulo.hpp"
+#include "Redistribution.hpp"
+
+namespace TwoDLib {
+
+	class TransitionMatrix;
+
+
+	  /**
+	   * \brief Responsible for representing the density and deterministic evolution.
+	   *
+	   * This create an object that is similar in functionality to an Ode2DSystem, but for a group
+	   * of meshes, so that the densities are stored in a single array, with the aim of vectorizing an
+	   * Algorithm. You are strongly encouraged to test the Meshes and Mappings in a stand alone MeshAlgorithm simulation
+	   * as this algorithm has more extensive checking for consistency between mesh and mappings.
+	   */
+
+	class Ode2DSystemGroup {
+	public:
+
+		//! Standard Constructor
+		Ode2DSystemGroup
+		(
+			const std::vector<Mesh>&, 					       //!< A series of Mesh in the Python convention. Most models require a reversal bin that is not part of the grid. In that case it must be inserted into the Mesh by calling Mesh::InsertStationary. It is legal not to define an extra reversal bin, and use one of the existing Mesh cells at such, but in that case Cell (0,0) will not exist.
+			const std::vector< std::vector<Redistribution> >&, //!< A series of mappings from strip end to reversal bin
+			const std::vector< std::vector<Redistribution> >&  //!< A series of mappings from threshold to reset bin
+		);
+
+
+		//! Place all initial density in a cell with coordinates (m,i,j)
+		void Initialize(MPILib::Index, MPILib::Index,MPILib::Index);
+
+		//! Map coordinates to a position in the density array. Map(0,0) may be defined or not, this depends on the Mesh and whether Mesh::InsertStationary
+		//! was called. The safe way to use Map is in a loop that uses Mesh::NrCellsInStrip and Mesh::NrQuadrilateralStrips.
+		MPILib::Index Map(
+				unsigned int kmesh,   //! mesh number
+				unsigned int istrip,  //! strip number
+				unsigned int jcell    //! cell number
+				) const {return _map[kmesh][istrip][jcell];}
+
+		//! Takes an index for the mass array and converts to the mapped version. This is used in the Master equation solvers
+		MPILib::Index Map(MPILib::Index i) const{ return _linear_map[i]; }
+
+		//! Shift the density
+		void Evolve();
+
+		//! Dump the current density profile (0), or the mass profile (1) to an output stream
+		void Dump(const std::vector<std::ostream*>&, int mode = 0) const;
+
+		//! Redistribute probability that has moved through threshold. Run this after the Master equation
+		void RedistributeProbability();
+
+		//! Remap probability that has run from the end of a strip. Run this after evolution
+		void RemapReversal();
+
+		//! Return the instantaneous firing rate
+		const vector<double>& F() const {return _fs;}
+
+		//! total probability mass in the system, should not be too far away from 1.0
+		double P() const { return std::accumulate(_vec_mass.begin(),_vec_mass.end(),0.0); }
+
+		//! average membrane potential, used for non-threshold crossing models such as Fitzhugh-Nagumo
+		const vector<double>& AvgV() const ;
+
+		//! allow direct inspection of the mass array; client must still convert this to a density
+		const vector<double>& Mass() const { return _vec_mass; }
+
+		//! allow inspection of the Mesh objects
+		const std::vector<Mesh>& MeshObjects() const { return _mesh_list; }
+
+		//! See what part of the mass array each Mesh is responsible for: the offsets are given as a function of mesh index
+		const std::vector<MPILib::Index>& Offsets() const {return _vec_mesh_offset; }
+
+		friend class Master;
+	    friend class MasterOMP;
+	    friend class MasterOdeint;
+
+	    friend void CheckSystem(const Ode2DSystemGroup&, const TransitionMatrix&, const std::vector<Redistribution>&, const std::vector<Redistribution>&, double);
+
+	private:
+
+		//! Copy constructor
+		Ode2DSystemGroup(const Ode2DSystemGroup&);
+
+		//! Implement the remapping of probability mass that hits the end of a strip
+		class Reversal {
+		public:
+
+			Reversal(Ode2DSystemGroup& sys, vector<double>& vec_mass, MPILib::Index m):_sys(sys),_vec_mass(vec_mass),_m(m){}
+
+			void operator()(const Redistribution& map){
+				_vec_mass[_sys.Map(_m,map._to[0],map._to[1])] += _vec_mass[_sys.Map(_m,map._from[0],map._from[1])];
+				_vec_mass[_sys.Map(_m,map._from[0],map._from[1])] = 0;
+			}
+
+		private:
+			MPILib::Index     _m;
+			Ode2DSystemGroup& _sys;
+			vector<double>&   _vec_mass;
+		};
+
+		//! Implement the remapping of probability mass that hits threshold
+		class Reset {
+		public:
+			Reset(Ode2DSystemGroup& sys, vector<double>& vec_mass,MPILib::Index m):_sys(sys),_vec_mass(vec_mass),_m(m){}
+
+			void operator()(const Redistribution& map){
+				double from =  map._alpha*_vec_mass[_sys.Map(_m,map._from[0],map._from[1])];
+				_vec_mass[_sys.Map(_m,map._to[0],map._to[1])] += from;
+				_sys._fs[_m] += from;
+			}
+
+		private:
+
+			MPILib::Index       _m;
+			Ode2DSystemGroup&	_sys;
+			vector<double>&     _vec_mass;
+		};
+
+		//! Implement cleaning of the probability that was at threshold. TODO: this is mildly inefficient,
+		//! in a serial implementation, but seems simpler in threads
+
+		class Clean {
+		public:
+
+			Clean(Ode2DSystemGroup& sys, vector<double>& vec_mass, MPILib::Index m):_sys(sys),_vec_mass(vec_mass),_m(m){
+			}
+
+			void operator()(const Redistribution& map){
+				_vec_mass[_sys.Map(_m, map._from[0],map._from[1])] = 0.;
+			}
+
+		private:
+
+			MPILib::Index       _m;
+			Ode2DSystemGroup&	_sys;
+			vector<double>&	    _vec_mass;
+		};
+
+		vector<MPILib::Index> InitializeLength(const Mesh&) const;
+		std::vector<std::vector<MPILib::Index> > InitializeLengths(const std::vector<Mesh>&);
+		std::vector<MPILib::Index> InitializeCumulative(const Mesh&) const;
+		std::vector<std::vector<MPILib::Index> > InitializeCumulatives(const std::vector<Mesh>&);
+		std::vector<MPILib::Number> MeshOffset(const std::vector<Mesh>&) const;
+
+		std::vector<Reset>    InitializeReset();
+		std::vector<Reversal> InitializeReversal();
+		std::vector<Clean>    InitializeClean();
+
+		vector<double>        InitializeArea(const std::vector<Mesh>&) const;
+		vector<double>        InitializeMass() const;
+
+		std::vector< std::vector< std::vector<MPILib::Index> > > InitializeMap() const;
+		std::vector< MPILib::Index> InitializeLinearMap();
+		void                  UpdateMap();
+
+		const std::vector<Mesh>&    _mesh_list;
+
+		std::vector<MPILib::Number>              _vec_mesh_offset;
+		std::vector<std::vector<MPILib::Index> > _vec_length;
+		std::vector<std::vector<MPILib::Index> > _vec_cumulative;
+
+		vector<double>	      _vec_mass;
+		vector<double>		  _vec_area;
+
+		unsigned int				_t;
+		std::vector<double>			_fs;
+		std::vector<double>         _avs;
+
+		std::vector< std::vector<std::vector<MPILib::Index> > > _map;
+		std::vector< MPILib::Index> _linear_map;
+
+		std::vector<std::vector<Redistribution> > _vec_reversal;
+		std::vector<std::vector<Redistribution> > _vec_reset;
+
+		std::vector<Reversal> _reversal;
+		std::vector<Reset>    _reset;
+		std::vector<Clean>    _clean;
+	};
+}
+#endif // include guard
