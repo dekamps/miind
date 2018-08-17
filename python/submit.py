ca3a4533f7d2505ba3644bfcd4a512319ee19f71
Marc de Kamps <scsmdk@leeds.ac.uk>
2018-08-17 10:52:26 +0100

    modified:   submit.py

--- python/submit.py
+++ python/submit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import directories
+import directories3 as directories
 import sys
 import os
 import subprocess
@@ -24,29 +24,28 @@ if __name__ == "__main__":
     # this gives the user the opportunity to adapt sub.sh to the local queueing system
     subpath = os.path.join(directories.miind_root(),'python')
 
-
     # either there are two arguments
     if len(sys.argv) == 1 or  len(sys.argv) > 4:
-	print 'Either a single argument containing the local submission directory, which is where the executable will be run, or'
-	print 'two arguments, which forces queue submission, where the second is the directory where submit.py is called from, or '
-	print 'three arguments with the third argument a local filename containing and alternative script for sub.sh'
-	sys.exit()
+        print ('Either a single argument containing the local submission directory, which is where the executable will be run, or')
+        print ('two arguments, which forces queue submission, where the second is the directory where submit.py is called from, or ')
+        print ('three arguments with the third argument a local filename containing and alternative script for sub.sh')
+        sys.exit()
 
     dir=sys.argv[1]
 
     if len(sys.argv) == 2:
-	local = True
-	queue = False
+        local = True
+        queue = False
 
     if len(sys.argv) == 3:
-	queue = True	
-	local = False
+        queue = True	
+        local = False
         calldir = sys.argv[2] 
         if not os.path.exists(os.path.join(calldir,"sub.sh")):
             subprocess.call(["cp",os.path.join(subpath,"sub.sh"),calldir])    
 
     if len(sys.argv) == 4:
-	raise UnImplemented
+        raise UnImplemented
 
 
     # or there are 3/4 with the thirds argument indicating that the submission script will be used for queue submission
@@ -63,16 +62,16 @@ if __name__ == "__main__":
             for fi in files:
                 with cd(fi):
                     localfiles=subprocess.check_output(["ls"]).split()
-                    
-                    if 'CMakeLists.txt' in localfiles:
+                    # subprocess returns bytes, and this difference is important in Python 3
+                    if b'CMakeLists.txt' in localfiles:
                         subprocess.call(['cmake','-DCMAKE_BUILD_TYPE=Release', '.'])
                         subprocess.call(['make'])
 			# copy the submission shells script from the calling directory
 			# this job could have done that directly, but this way the user can adapt the submission script in the calling directory
-			subprocess.call(['cp',os.path.join(subpath,'sub.sh'),'.'])
+                        subprocess.call(['cp',os.path.join(subpath,'sub.sh'),'.'])
                         # file name is the same as directory name, if miind was called
-                        print fi
-			if local == True:
-                            subprocess.call('./' + fi)
-			if queue == True:
-			    subprocess.call(['qsub', 'sub.sh', './' + fi])
+                    
+                        if local == True:
+                            subprocess.call('./' + fi.decode('ascii'))
+                        if queue == True:
+                            subprocess.call(['qsub', 'sub.sh', './' + fi.decode('ascii')])
