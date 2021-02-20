3cf9b2626619643a68ee9739d54d7d58f51e54fa
Hugh Osborne <hugh.osborne@gmail.com>
2021-02-20 16:21:19 +0000

    OK, so, the triplet files can have specific definitions of static or dynamic. lets try this.

---
+++ custom-triplets/x64-linux-mixed.cmake
@@ -0,0 +1,15 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+if(PORT STREQUAL "boost")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+if(PORT STREQUAL "freeglut")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+if(PORT STREQUAL "glew")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
---
+++ custom-triplets/x64-osx-mixed.cmake
@@ -0,0 +1,16 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES x86_64)
+
+if(PORT STREQUAL "boost")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+if(PORT STREQUAL "freeglut")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+if(PORT STREQUAL "glew")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
