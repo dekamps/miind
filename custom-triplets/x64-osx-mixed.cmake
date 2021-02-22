f312afa271e0ff9f55a3527bdd3d9d062a477b9e
Hugh Osborne <hugh.osborne@gmail.com>
2021-02-22 08:53:26 +0000

    selecting specific boost libraries was more hastle than it was worth.

--- custom-triplets/x64-osx-mixed.cmake
+++ custom-triplets/x64-osx-mixed.cmake
@@ -7,78 +7,8 @@ set(VCPKG_OSX_ARCHITECTURES x86_64)
 
 message( "Testing PORT ${PORT}." )
 
-#if(PORT MATCHES "boost")
-#	message ( " This boost lib should be dynamic.")
-#    set(VCPKG_LIBRARY_LINKAGE dynamic)
-#endif()
-
-if(PORT MATCHES "boost-test")
-	message ( " This boost-test lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-numeric-conversion")
-	message ( " This boost-numeric-conversion lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-odeint")
-	message ( " This boost-odeint lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-filesystem")
-	message ( " This boost-filesystem lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-program-options")
-	message ( " This boost-program-options lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-system")
-	message ( " This boost-system lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-thread")
-	message ( " This boost-thread lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-timer")
-	message ( " This boost-timer lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-serialization")
-	message ( " This boost-serialization lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-chrono")
-	message ( " This boost-chrono lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-date-time")
-	message ( " This boost-date-time lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-atomic")
-	message ( " This boost-atomic lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-core")
-	message ( " This boost-core lib should be dynamic.")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if(PORT MATCHES "boost-python")
-	message ( " This boost-python lib should be dynamic.")
+if(PORT MATCHES "boost")
+	message ( " This boost lib should be dynamic.")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
