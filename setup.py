7e0e95a71caf311822ede9fec453cd4efa3914a0
Hugh Osborne <hugh.osborne@gmail.com>
2021-02-24 12:41:46 +0000

    hmm

--- setup.py
+++ setup.py
@@ -160,6 +160,7 @@ def main():
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "License :: OSI Approved :: MIT License",
             #"Operating System :: MacOS",
             "Operating System :: Unix",
