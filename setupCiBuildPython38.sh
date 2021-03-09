08b65f3dfa2c2b178130f4c437f0b00def3322bf
Hugh Osborne <hugh.osborne@gmail.com>
2021-03-09 07:18:48 +0000

    oionhfidsnfo

---
+++ setupCiBuildPython38.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Manylinux2014 yum cannot install python3-devel so for python 3.8 and 3.9, we need to manually download, build from source, and install to get access to Python.h for the extension."
+
+yum install gcc openssl-devel bzip2-devel libffi-devel -y
+curl -O https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
+tar -xzf Python-3.8.1.tgz
+cd Python-3.8.1/
+./configure --enable-optimizations
+make install
+
+
