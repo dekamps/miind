1f3af379b6dd6022328f721fa60c8daa181512e6
Hugh Osborne <hugh.osborne@gmail.com>
2021-05-17 19:26:12 +0100

    try without python build

--- setupCiBuildPython.sh
+++ setupCiBuildPython.sh
@@ -13,36 +13,3 @@ parsedVersionRed=$(echo "${parsedVersion:0:3}")
 
 echo "Found Python Version: $parsedVersionRed"
 
-if [[ "$parsedVersionRed" -gt "370" && "$parsedVersionRed" -lt "380" ]]
-then 
-    yum install gcc openssl-devel bzip2-devel libffi-devel -y
-	curl -O https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tgz
-	tar -xzf Python-3.7.1.tgz
-	cd Python-3.7.1/
-	./configure --enable-optimizations
-	make install
-	updatedb
-fi
-
-if [[ "$parsedVersionRed" -gt "380" && "$parsedVersionRed" -lt "390" ]]
-then 
-    yum install gcc openssl-devel bzip2-devel libffi-devel -y
-	curl -O https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
-	tar -xzf Python-3.8.1.tgz
-	cd Python-3.8.1/
-	./configure --enable-optimizations
-	make install
-	updatedb
-fi
-
-if [[ "$parsedVersionRed" -gt "390" && "$parsedVersionRed" -lt "400" ]]
-then 
-    yum install gcc openssl-devel bzip2-devel libffi-devel -y
-	curl -O https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
-	tar -xzf Python-3.9.1.tgz
-	cd Python-3.9.1/
-	./configure --enable-optimizations
-	make install
-	updatedb
-fi
-
